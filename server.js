b6c37341cfb96957b6919ebcce9f6c57ef3267f9
Sethmh82 <seth@statisticbrain.com>
2017-07-02 18:06:13 -0700

    commit new seq files

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-var numbers = [1,2,4]
-var city = "newport";
+// VARIABLES
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var db = require("./models");
 
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect();
 
-var object = {
-    theArray: numbers,
-    //or
-    numbers,
-    city
-};
+var PORT = process.env.PORT || 3000;
 
-res.render("index", object);
+// EXPRESS
+var app = express();
+app.use(express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
+// EXPRESS HANDLEBARS
+var exphand = require("express-handlebars");
+app.engine("handlebars", exphand({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// ROUTES
+var burgerRoutes = require("./controllers/burgers_controllers.js");
+app.use(burgerRoutes);
+
+// PORT LISTENER APP.LISTEN
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() { console.log("App listening on PORT " + PORT); });
+});
